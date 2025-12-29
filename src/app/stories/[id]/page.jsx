8ffb92845ceb2e1e789c08dfa8119a6e05147d0d
chayansrk64 <chayansrk64@gmail.com>
2025-12-29 12:48:21 +0600

    stories details page added by dynamic routes

--- src/app/stories/[id]/page.jsx
+++ src/app/stories/[id]/page.jsx
@@ -1,8 +1,7 @@
-import StoryCard from '@/components/StoryCard';
-import Title from '@/components/Title';
 import React from 'react';
 
-const Stories = () => {
+const StoryDetailsPage = async({params}) => {
+    const {id} = await params;
     const devStories = [
   {
     "id": 1,
@@ -61,17 +60,58 @@ const Stories = () => {
   }
 ]
 
+    const story = devStories.find(story=> story.id == id)
+    if(!story){
+        return <h2>Story not found!</h2>
+    }
+
     return (
         <div>
-            <Title>All the Stories are here...</Title>
+            <div className="max-w-3xl mx-auto  shadow-md rounded-lg p-6 border border-gray-200">
+      {/* Top Section */}
+      <div className="flex items-center gap-5">
+        <img
+          src={story.image}
+          alt={story.name}
+          className="w-24 h-24 rounded-full object-cover"
+        />
+        <div>
+          <h1 className="text-2xl font-semibold">{story.name}</h1>
+          <p className="text-gray-700">{story.designation}</p>
+          <p className="text-blue-600">{story.company}</p>
+          <p className="text-gray-600 mt-1">Experience: {story.experience}</p>
+        </div>
+      </div>
 
-            <div className='mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                {
-                    devStories.map(story=> <StoryCard key={story.id} story={story}></StoryCard>)
-                }
-            </div>
+      {/* Story */}
+      <div className="mt-6">
+        <h2 className="text-xl font-semibold mb-2">Learning Story</h2>
+        <p className="text-gray-700 leading-relaxed">{story.story}</p>
+      </div>
+
+      {/* About */}
+      <div className="mt-6">
+        <h2 className="text-xl font-semibold mb-2">About Developer</h2>
+        <p className="text-gray-700 leading-relaxed">{story.about}</p>
+      </div>
+
+      {/* Skills */}
+      <div className="mt-6">
+        <h2 className="text-xl font-semibold mb-2">Skills</h2>
+        <div className="flex flex-wrap gap-2">
+          {story.skills.map((skill, index) => (
+            <span
+              key={index}
+              className="bg-gray-200 px-3 py-1 rounded-full text-sm"
+            >
+              {skill}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
         </div>
     );
 };
 
-export default Stories;
+export default StoryDetailsPage;
